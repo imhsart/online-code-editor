bf8c5526d47ef67323346035f81bff05422a797a
Nitin <nitinrana2519@gmail.com>
2026-03-19 21:28:18 +0530

    Made the both sections resizable using DOM and mouse events

---
+++ script.js
@@ -0,0 +1,23 @@
+let container = document.querySelector('.container')
+let qSection = document.querySelector('#question-section')
+let eSection = document.querySelector('#editor-section')
+let ruler = document.querySelector('#ruler')
+
+let isDragging = false
+let containerWidth 
+let containerLeft
+ruler.addEventListener('mousedown', () => {
+  isDragging = true
+  containerWidth = container.getBoundingClientRect().width
+  containerLeft = container.getBoundingClientRect().left
+})
+
+document.addEventListener('mousemove', (e) => {
+  if(isDragging){
+    let pointerVal = e.clientX - containerLeft
+    qSection.style.width = `clamp(50px, ${pointerVal}px, ${containerWidth-50}px)`
+  }
+})
+document.addEventListener('mouseup', () => {
+  isDragging = false
+})
