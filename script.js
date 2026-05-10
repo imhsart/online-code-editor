a485c442f9efbcee3150fd4049bf248544530e03
imhsart <nitinrana2519@gmail.com>
2026-05-10 15:41:18 +0530

    Miscellaneous commit

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let qSection = document.querySelector('#question-section')
 let eSection = document.querySelector('#editor-section')
 let ruler = document.querySelector('#ruler')
 let overlay = document.querySelector('.overlay')
+let getExpectedValue = document.getElementById('expected-btn')
+let submitBtn = document.getElementById('submit-btn')
 
 let isDragging = false
 let containerWidth 
@@ -25,3 +27,18 @@ document.addEventListener('mouseup', () => {
   isDragging = false
   overlay.style.display = 'none'
 })
+
+
+let latestCode = null;
+
+//reading message from editor
+// window.addEventListener('message', (e) => {
+//   console.log(e)
+//     if (e.data && e.data.language) {
+//         latestCode = e.data;
+//     }
+// });
+
+submitBtn.addEventListener('click', () => {
+    console.log("Submitting:", latestCode);
+});
